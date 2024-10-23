fa53ac88cf37a38446fbdeb88c58b61544ed3af3
ahmedachora10 <gjr8195@gmail.com>
2024-10-23 16:14:47 +0100

    fix components issues

--- Modules/Dashboard/app/View/Components/Sidebar/Link.php
+++ Modules/Dashboard/app/View/Components/Sidebar/Link.php
@@ -3,13 +3,12 @@
 namespace Modules\Dashboard\App\View\Components\Sidebar;
 
 use Illuminate\View\Component;
+use Illuminate\View\View;
 
 class Link extends Component
 {
     /**
      * Create a new component instance.
-     *
-     * @return void
      */
     public function __construct(public $link = 'javascript:viod(0)', public $icon = '', public $title = null, public bool $hasSubMenu = false, public int|bool $notification = false)
     {
@@ -17,12 +16,10 @@ class Link extends Component
     }
 
     /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     * Get the view/contents that represent the component.
      */
-    public function render()
+    public function render(): View|string
     {
-        return view('dashboard::components.sidebar.link');
+        return view('dashboard::components.sidebar/link');
     }
 }
